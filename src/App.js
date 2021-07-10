cafdaf31141cf36d385b217e71dacea996a33cd5
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-10 17:05:19 -0400

    Add a button to toggle theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookList from './components/BookList'
 import Navbar from './components/Navbar'
+import ThemeToggle from './components/ThemeToggle'
 import ThemeContextProvider from './contexts/ThemeContext'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
       
     </div>
