37e64039ee45186c9b08f4b57bc9cbf1fbbaca12
Jiayi Su <sjy.su@mail.utoronto.ca>
2021-07-10 16:55:18 -0400

    Another way: Consumer to use context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,26 @@ import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
   // state = {  }
-  static contextType = ThemeContext
   render() { 
-    console.log(this.context)
-    const { isLightTheme, light, dark } = this.context
-    const theme = isLightTheme ? light : dark
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context
+        const theme = isLightTheme ? light : dark
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax}}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+        
+      }}
+
+      </ThemeContext.Consumer>
+      
     );
   }
 }
